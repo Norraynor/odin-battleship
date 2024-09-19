61f580b3c95ba0816c7bfd5a78515a4f0d60927e
norraynor <37270508+Norraynor@users.noreply.github.com>
2024-09-19 20:32:21 +0200

    Add tests and function to place ship

--- modules/gameboard.js
+++ modules/gameboard.js
@@ -1,25 +1,35 @@
 function Gameboard() {
-	const boardSize = 10;
+	const boardSize = 3;
 	function createBoard(size) {
 		let board = Array.from({ length: size }, () => Array(size).fill(null));
 		return board;
 	}
-	const gameboard = createBoard(3);
+	const gameboard = createBoard(boardSize);
 	function placeShip(ship, x, y, horizontal = true) {
-		gameboard[x][y] = ship;
+		//gameboard[x][y] = ship;
 		if (horizontal) {
 			if (x + ship.getLength() <= gameboard.length) {
 				//place ship
+				for (i = x; i < x + ship.getLength(); i++) {
+					gameboard[i][y] = ship;
+				}
 			} else {
 				return "invalid placement";
 			}
 		}
 	}
-	//test board
-	//let board = createBoard(3);
+	function receiveAttack(x, y) {
+		if (gameboard[x][y] == null) {
+			return "miss";
+		} else {
+			gameboard[x][y].hit();
+			return gameboard[x][y].hitPoints();
+		}
+	}
 	return {
 		gameboard,
 		placeShip,
+		receiveAttack,
 	};
 }
 
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,13 +1,19 @@
-const gameboard = require("../modules/gameboard.js");
+const board = require("../modules/gameboard.js");
 const ship = require("../modules/ship.js");
 
 describe("gameboard object tests", () => {
-	let currentGameboard = gameboard.Gameboard();
+	let currentGameboard = board.Gameboard();
 	let emptyArray = [
 		[null, null, null],
 		[null, null, null],
 		[null, null, null],
 	];
+	beforeEach(() => {
+		currentGameboard.gameboard = emptyArray;
+	});
+	afterEach(() => {
+		currentGameboard.gameboard = emptyArray;
+	});
 	test("gameboard exists", () => {
 		expect(currentGameboard).toBeDefined();
 	});
@@ -20,5 +26,27 @@ describe("gameboard object tests", () => {
 		gameArray[0][0] = newShip;
 		currentGameboard.placeShip(newShip, 0, 0);
 		expect(currentGameboard.gameboard[0][0]).toStrictEqual(gameArray[0][0]);
+	});
+	test("ship takes correct amount of space on board", () => {
+		let newShip = ship.Ship(2);
+		let gameArray = emptyArray;
+		gameArray[0][0] = newShip;
+		gameArray[1][0] = newShip;
+		currentGameboard.placeShip(newShip, 0, 0);
+		expect(currentGameboard.gameboard[0][0]).toStrictEqual(gameArray[0][0]);
+		expect(currentGameboard.gameboard[1][0]).toStrictEqual(gameArray[1][0]);
+	});
+	test("board receive attack", () => {
+		expect(currentGameboard.receiveAttack(2, 0)).toBe("miss");
+	});
+	test("board receive attack and ship sink", () => {
+		let newShip = ship.Ship(2);
+		currentGameboard.placeShip(newShip, 0, 0);
+		expect(currentGameboard.gameboard[0][0].hitPoints()).toBe(2);
+		//currentGameboard.receiveAttack(0, 0);
+		// expect(currentGameboard.gameboard[0][0].hitPoints()).toBe(1);
+		expect(currentGameboard.receiveAttack(0, 0)).toBe(1);
+		expect(currentGameboard.receiveAttack(0, 0)).toBe(0);
+		expect(currentGameboard.gameboard[0][0].hitPoints()).toBe(0);
 	});
 });
